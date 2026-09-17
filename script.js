303049e326a902a04d96e4dc1c45bffaf6c6578f
noledinyjnepobedimyj-star <noledinyjnepobedimyj@gmail.com>
2026-09-17 21:34:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.main-nav');
+
+menuToggle?.addEventListener('click', () => {
+  const isOpen = nav.classList.toggle('open');
+  menuToggle.setAttribute('aria-expanded', String(isOpen));
+});
+
+document.querySelectorAll('.main-nav a').forEach((link) => {
+  link.addEventListener('click', () => {
+    nav.classList.remove('open');
+    menuToggle.setAttribute('aria-expanded', 'false');
+  });
+});
